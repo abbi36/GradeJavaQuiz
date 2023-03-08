9770d17df6ae8d22bcb7ff0dbf4a20310fba225e
Abbi Engel <abbi.engel33@gmail.com>
2023-03-08 09:58:47 -0600

    Quiz Grade Scores

--- src/Grades.java
+++ src/Grades.java
@@ -1,94 +1,63 @@
-import java.util.*;
-
-
 import java.util.Scanner;
 public class Grades {
+
     public static void main(String[] args) {
-        System.out.println("Please enter quiz grade 0 to 100.");
-        System.out.println("To end the program enter 999.");
-        Scanner input = new Scanner(System.in);
-        int gradePercent = input.nextInt();
-        int [] grade = new int[gradePercent];
-
-        while(gradePercent >=10) {
-            System.out.println("Enter the grade: ");
-        }\
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         {
             System.out.println("Please enter test scores that range from 0 to 100.");
-            System.out.println("To end the program enter 999.");
-            System.out.println();  // print a blank line
             Scanner sc = new Scanner(System.in);
             String choice = "y";
 
 
-            while (!choice.equalsIgnoreCase("n"))
-            {
+            while (!choice.equalsIgnoreCase("n")) {
                 int scoreTotal = 0;
                 int scoreCount = 0;
                 int testScore = 0;
-                System.out.println("Enter the number of test score to be entered: ");
+                String letterGrade = "";
+                System.out.println("Enter the number of test scores to be entered: ");
                 int numberOfTestScores = sc.nextInt();
 
-                for (int i = 1; i <= numberOfTestScores; i++)
-                {
+                for (int i = 1; i <= numberOfTestScores; i++) {
 
                     System.out.print("Enter score " + i + ": ");
                     testScore = sc.nextInt();
 
-                    if (testScore <= 100)
-                    {
+                    if (testScore <= 100) {
                         scoreCount = scoreCount + 1;
                         scoreTotal = scoreTotal + testScore;
 
 
-                    }
-                    else if (testScore > 100)
+                    } else if (testScore > 100)
                         System.out.println("Invalid entry, not counted");
                 }
 
-                double averageScore = scoreTotal / scoreCount;
+                int averageScore = scoreTotal / scoreCount;
+                if (averageScore >= 90) {
+                   letterGrade = "A";
+                } else if (averageScore >= 80 && averageScore <90 ) {
+                    letterGrade = "B";
+                } else if (averageScore >=70 && averageScore <80) {
+                    letterGrade = "C";
+                } else if (averageScore >=60 && averageScore <70) {
+                    letterGrade = "D";
+                } else {
+                    letterGrade = "F";
+                }
+
+
+
+
+
                 String message = "\n" +
                         "Score count:   " + scoreCount + "\n"
                         + "Score total:   " + scoreTotal + "\n"
-                        + "Average score: " + averageScore + "\n";
+                        + "Average score: " + averageScore + "\n"
+                        + "You earned  a " +letterGrade + "!!! And, shout out to Katy, my inspiration!";
                 System.out.println(message);
-                System.out.println();
                 System.out.println("Enter more test scores? (y/n)");
-                choice= sc.next();
+                choice = sc.next();
             }
+
         }
     }
 }
-
-
-
-
-
-
-
